548ba98c7ec40b07b7698cf2d881e8a09b70f270
alessio-katreli <91191630+alessio-katreli@users.noreply.github.com>
2023-12-20 09:47:13 +0000

    test-routing angular 17

---
+++ test-routing/src/app/vista-1/vista-1.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-vista-1',
+  standalone: true,
+  imports: [],
+  templateUrl: './vista-1.component.html',
+  styleUrl: './vista-1.component.css'
+})
+export class Vista1Component {
+
+}
---
+++ test-routing/src/app/vista-3/vista-3.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-vista-3',
+  standalone: true,
+  imports: [],
+  templateUrl: './vista-3.component.html',
+  styleUrl: './vista-3.component.css'
+})
+export class Vista3Component {
+
+}
